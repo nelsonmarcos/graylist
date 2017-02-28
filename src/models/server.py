93eb70c3c3891a9a148103df6e411be48d036fe6
Nelson Marcos <nelson.marcos@corp.globo.com>
2017-02-27 22:02:07 -0300

    First commit

---
+++ src/models/server.py
@@ -0,0 +1,8 @@
+class Server(object):
+
+    def __init__(self, name, env, instance, _id):
+        self.name = name
+        self.env = env
+        self.instance = instance
+        self.id = _id
+
